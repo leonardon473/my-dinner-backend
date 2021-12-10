7e12be6cd80fca93bff355bd5ad83a8435741de8
Leonardo Ramírez <leonardon473@gmail.com>
2021-12-10 11:52:38 -0600

    added: Redoc is added.

--- src/django_core/urls.py
+++ src/django_core/urls.py
@@ -1,7 +1,22 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 
 urlpatterns = [
+    path(
+        "openapi",
+        get_schema_view(title="My Dinner", version="1.0.0"),
+        name="openapi-schema",
+    ),
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc.html", extra_context={"schema_url": "openapi-schema"}
+        ),
+        name="redoc",
+    ),
+    # rest apis
     path("orders/", include("apps.orders.urls")),
     path("users/", include("apps.users.urls")),
     path("menu/", include("apps.menu.urls")),
