86610a4ff9ec460b88002104d46389a3cc18d7eb
Leonardo Ramírez <leonardon473@gmail.com>
2021-12-09 23:12:01 -0600

    added: CRUD for Client Address model is added.

---
+++ src/apps/users/serializers/client_address.py
@@ -0,0 +1,52 @@
+# -----------------------------------------------------------------------------
+# Libraries
+# -----------------------------------------------------------------------------
+# Core libs
+from typing import TYPE_CHECKING
+
+# Third party libs
+from rest_framework import serializers
+
+# Project libs
+from apps.users.models import ClientAddress
+
+# If type checking, __all__
+if TYPE_CHECKING:
+    pass
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+
+
+class ClientAddressCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = [
+            "client",
+            "street",
+            "num_ext",
+            "num_int",
+            "neighborhood",
+            "zip_code",
+        ]
+        model = ClientAddress
+
+
+class ClientAddressRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = [
+            "street",
+            "num_ext",
+            "num_int",
+            "neighborhood",
+            "zip_code",
+        ]
+        model = ClientAddress
--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views.rest import client
+from .views.rest import client_address
 
 urlpatterns = [
     path("clients/", client.CreateListUserView.as_view()),
     path("clients/<int:pk>/", client.UserRetrieveUpdateView.as_view()),
+    path("clients-addresses/", client_address.ClientAddressCreateListView.as_view()),
+    path(
+        "clients-addresses/<int:pk>/",
+        client_address.ClientAddressRetrieveUpdateView.as_view(),
+    ),
 ]
