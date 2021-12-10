c3f141508932ea62a3e58ce8c879c4a837dbd114
Leonardo Ramírez <leonardon473@gmail.com>
2021-12-10 13:08:12 -0600

    added: Client password is hashed.

--- src/apps/users/models/client.py
+++ src/apps/users/models/client.py
@@ -27,11 +27,27 @@ if TYPE_CHECKING:
 # -----------------------------------------------------------------------------
 
 
+class ClientManager(models.Manager):
+    def create(self, **kwargs):
+        """
+        Create a new object with the given kwargs, saving it to the database
+        and returning the created object.
+        """
+        password = kwargs.pop("password")
+        obj = self.model(**kwargs)
+        obj.set_password(password)
+        self._for_write = True
+        obj.save(force_insert=True, using=self.db)
+        return obj
+
+
 class Client(AbstractBaseUser, models.Model):
     client_id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=120, unique=True)
     full_name = models.CharField(max_length=120)
     mobile_number = models.CharField(max_length=20)
+
+    objects = ClientManager()
 
     class Meta:
         verbose_name = "Client"
